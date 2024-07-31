40643fc584d6da5eb809c28aecc6d3a0e075e818
amytran <167041446+amytran001@users.noreply.github.com>
2024-07-31 15:37:01 -0700

    Create Couple.java

---
+++ Couple.java
@@ -0,0 +1,15 @@
+public class Couple {
+    private Person bride;
+    private Person groom;
+    public Couple(Person br, Person gr){
+        bride = br;
+        groom = gr;
+    }
+    public Person getBride(){
+        return bride;
+    }
+    public Person getGroom(){
+        return groom;
+    }
+
+}
