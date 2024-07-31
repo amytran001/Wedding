01fe1577c048d3fc705c21cc98cb80cadb6a4a56
amytran <167041446+amytran001@users.noreply.github.com>
2024-07-31 15:36:38 -0700

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    public Person (String first, String last){
+        firstName = first;
+        lastName = last;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+}
